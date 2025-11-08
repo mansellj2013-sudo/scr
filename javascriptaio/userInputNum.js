30f612f8f8fef604335b3814229686ef16657e5e
Jonathan Mansell <mansellj2013@gmail.com>
2025-11-07 23:09:16 -0500

    added 2 examples from CH-3

---
+++ javascriptaio/userInputNum.js
@@ -0,0 +1,6 @@
+let firstNumber = prompt("Pick a number");
+let firstNumFloat = parseFloat(firstNumber);
+let secondNumber = prompt("Pick another number");
+let secondNumFloat = parseFloat(secondNumber);
+let sum = firstNumFloat + secondNumFloat;
+alert(sum);
